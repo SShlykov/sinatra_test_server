884f217af3e55b5f89bf8d1998d1d05b8ae7b636
Sergey Shlykov <zeitment@gmail.com>
2020-12-12 15:55:36 +0300

    heroku

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ class Web < Sinatra::Base
   before do
     headers['X-Author'] = @@user
     headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Allow-Methods'] = "GET,POST,PUT,DELETE,OPTIONS"
+    headers['Access-Control-Allow-Headers'] = "x-test,Content-Type,Accept,Access-Control-Allow-Headers"
   end
   
   get /\/?/ do
@@ -26,7 +28,7 @@ class Web < Sinatra::Base
 
   get /\/promise\/?/ do
     headers['Content-Type'] = @@plain_text
-    "function task(x){ (value => new Promise((res, rej) => (value < 18 ? res(\"yes\") : rej(\"no\"))))(x) }"
+    "function task(x){ return new Promise((res, rej) => x < 18 ? res('yes') : rej('no')); }"
   end
   get /\/fetch\/?/ do
     erb :index
