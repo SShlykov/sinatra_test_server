2969d441dd961d5c4a4cbbeb1aee42bb717d0ae3
Sergey Shlykov <zeitment@gmail.com>
2020-12-12 16:37:36 +0300

    heroku

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require 'json'
 
 
 class Web < Sinatra::Base
   @@user = 'sshlykov'
   @@plain_text = "text/plain; charset=UTF-8"
+
+  result4 = lambda do
+    content_type :json
+    { message: @@user, "x-result": request.env["HTTP_X_TEST"], "x-body": request.body.read}.to_json
+  end
+
   
   before do
     headers['X-Author'] = @@user
@@ -30,7 +37,14 @@ class Web < Sinatra::Base
     headers['Content-Type'] = @@plain_text
     "function task(x){ return new Promise((res, rej) => x < 18 ? res('yes') : rej('no')); }"
   end
+  
   get /\/fetch\/?/ do
     erb :index
   end
+
+  get /\/result4\/?/, &result4
+  post /\/result4\/?/, &result4
+  put /\/result4\/?/, &result4
+  delete /\/result4\/?/, &result4
+  options /\/result4\/?/, &result4
 end
